9105fdee4419c2631e78f27b7d13a8afc444791c
Filip Dimitrijevic <dimitrijevicfilip995@gmail.com>
2021-06-04 17:51:32 +0200

    Getting Books of a Publisher including Authors

--- my-books-v2/Controllers/PublishersController.cs
+++ my-books-v2/Controllers/PublishersController.cs
@@ -26,5 +26,12 @@ namespace my_books_v2.Controllers
             _publisherService.AddPublisher(publisher);
             return Ok();
         }
+
+        [HttpGet("get-publisher-books-with-authors/{id}")]
+        public IActionResult GetPublisherData(int id)
+        {
+            var _response = _publisherService.GetPublisherData(id);
+            return Ok(_response);
+        }
     }
 }
