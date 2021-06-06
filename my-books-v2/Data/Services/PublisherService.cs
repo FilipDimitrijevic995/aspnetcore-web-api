341da0da55060a460abd2ec7149dab01eb2a6e26
Filip Dimitrijevic <dimitrijevicfilip995@gmail.com>
2021-06-06 10:34:13 +0200

    Adding Server Side Filtering

--- my-books-v2/Data/Services/PublisherService.cs
+++ my-books-v2/Data/Services/PublisherService.cs
@@ -15,7 +15,7 @@ namespace my_books_v2.Data.Services
             _context = context;
         }
 
-        public List<Publisher> GetAllPublishers(string sortBy) 
+        public List<Publisher> GetAllPublishers(string sortBy, string searchString) 
         {
             var allPublishers = _context.Publishers.OrderBy(n => n.Name).ToList();
 
@@ -29,6 +29,11 @@ namespace my_books_v2.Data.Services
                     default:
                         break;
                 }
+            }
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                allPublishers = allPublishers.Where(n => n.Name.Contains(searchString, StringComparison.CurrentCultureIgnoreCase)).ToList();
             }
 
             return allPublishers;
