05542a1294c44e078ee029e3ff09e0a8ddc8744a
Filip Dimitrijevic <dimitrijevicfilip995@gmail.com>
2021-10-18 11:54:02 +0200

    Unit Testing HttpGet in PublishersController

---
+++ my-books-v2-tests/PublishersControllerTests.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using my_books_v2.Controllers;
+using my_books_v2.Data;
+using my_books_v2.Data.Models;
+using my_books_v2.Data.Services;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_books_v2_tests
+{
+    public class PublishersControllerTests
+    {
+        private static DbContextOptions<AppDbContext> dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "BookDbContorollerTest")
+            .Options;
+
+        AppDbContext context;
+        PublisherService publishersService;
+        PublishersController publishersController;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            context = new AppDbContext(dbContextOptions);
+            context.Database.EnsureCreated();
+
+            SeedDatabase();
+
+            publishersService = new PublisherService(context);
+            publishersController = new PublishersController(publishersService);
+        }
+        [Test, Order(1)]
+        public void HTTPGET_GetAllPublishers_WithSortBySearchPageNr_ReturnOk_Test()
+        {
+            IActionResult actionResult = publishersController.GetAllPublishers("name_desc", "Publisher", 1);
+            Assert.That(actionResult, Is.TypeOf<OkObjectResult>());
+            var actionResultData = (actionResult as OkObjectResult).Value as List<Publisher>;
+            Assert.That(actionResultData.First().Name, Is.EqualTo("Publisher 6"));
+            Assert.That(actionResultData.First().Id, Is.EqualTo(6));
+            Assert.That(actionResultData.Count, Is.EqualTo(5));
+
+
+            IActionResult actionResultSecondPage = publishersController.GetAllPublishers("name_desc", "Publisher", 2);
+            Assert.That(actionResultSecondPage, Is.TypeOf<OkObjectResult>());
+            var actionResultDataSecondPage = (actionResultSecondPage as OkObjectResult).Value as List<Publisher>;
+            Assert.That(actionResultDataSecondPage.First().Name, Is.EqualTo("Publisher 1"));
+            Assert.That(actionResultDataSecondPage.First().Id, Is.EqualTo(1));
+            Assert.That(actionResultDataSecondPage.Count, Is.EqualTo(1));
+        }
+
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            context.Database.EnsureDeleted();
+        }
+
+        private void SeedDatabase()
+        {
+            var publishers = new List<Publisher>
+            {
+                    new Publisher() {
+                        Id = 1,
+                        Name = "Publisher 1"
+                    },
+                    new Publisher() {
+                        Id = 2,
+                        Name = "Publisher 2"
+                    },
+                    new Publisher() {
+                        Id = 3,
+                        Name = "Publisher 3"
+                    },
+                    new Publisher() {
+                        Id = 4,
+                        Name = "Publisher 4"
+                    },
+                    new Publisher() {
+                        Id = 5,
+                        Name = "Publisher 5"
+                    },
+                    new Publisher() {
+                        Id = 6,
+                        Name = "Publisher 6"
+                    },
+            };
+            context.Publishers.AddRange(publishers);
+
+            context.SaveChanges();
+        }
+
+    }
+}
